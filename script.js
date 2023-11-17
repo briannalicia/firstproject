c0cf302f6f153e6e4b2d3ac2f8de37e04600a6fd
Brianna Kennedy <briannaa.kennedy@gmail.com>
2023-11-16 19:23:22 -0500

    fifth push creating variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function changeImage(){
+function changeImage(evt){
     let displayImage = document.getElementById("image1")
     if (displayImage.src.match("./plain_donut.png")){
         displayImage.src = "./blue_donut.png"
@@ -6,3 +6,9 @@ function changeImage(){
         displayImage.src = "./plain_donut.png"
     }
 }
+
+const container = document.querySelector(".grid-container")
+container.addEventListener('click', function(evt){
+    console.log(evt.target)
+    changeImage(evt.target)
+})
